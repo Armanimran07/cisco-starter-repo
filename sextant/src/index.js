ae49ade1a89b9332e5f3fea05973e099978242ed
Muhammad Imran <imrannajar05@gmail.com>
2023-02-03 01:43:52 +0530

    sextant

--- sextant/src/index.js
+++ sextant/src/index.js
@@ -4,12 +4,42 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// banner start component 
+
+const Banner = () => (
+  <header>
+    <h1>Welcome To Cisco</h1>
+  </header>
+);
+
+export default Banner;
+
+// banner component end 
+
+// exhibit component start
+
+const Exhibit = ({ heading, children }) => {
+  return (
+    <div className="exhibit">
+      <h3 className="exhibit-heading">{heading}</h3>
+      <div className="exhibit-children">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+// exhibit component end 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
